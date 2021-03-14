ad3039e6c1633457018adf87d4a4df7f46c2c3b4
Evgeniy <yevgeniykyr5@ukr.netgit config --global user.name prostoevgeniy5git config --global user.email yevgeniykyr5@ukr.net>
2021-03-14 14:28:08 +0200

    feat: solution of task towel-sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,36 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(matrix === undefined || Array.isArray(matrix) && (matrix.length === 0)) {
+    return [];
+  }
+  let temp = [];
+//   let result;
+//   temp = matrix.map((el, ind) => {
+//       let res;
+//       if (ind % 2 !== 0) {
+//           res = el.map((el) => el).reverse() 
+//       } else if (ind % 2 === 0) {
+//           res = el.map(el => el)
+//       }
+//       return +res.join('');
+//   })
+
+    temp = matrix.reduce((ac, el, ind) => {
+        if(ind % 2 === 0) {
+            el.forEach(elem => {
+                ac.push(elem)
+            })
+            return ac
+        } else if(ind % 2 !== 0) {
+            el = el.reverse();
+            el.forEach(elem => {
+                ac.push(elem)
+            })
+            return ac
+        }
+    }, []);
+
+  return temp;
+
 }
